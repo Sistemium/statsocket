528d853f0bb3f74ccc7b132ea0498ad22975d836
Denis Mosin <deniss.mosin@gmail.com>
2015-08-14 15:47:09 +0300

    socket connection

--- app/index.js
+++ app/index.js
@@ -1,3 +1,37 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var status = require('./status.js');
+
+app.get('/', function(req, res){
+  res.send('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  status.get(function (data){
+    socket.emit('news', {
+      type:'welcome',
+      status: data
+    });
+  });
+
+  socket.on('my other event', function(data) {
+    console.log(data);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
 module.exports = function () {
   return 'Hello, world';
 };
