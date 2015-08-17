a49d38132e6081f4c41fc9c2a62391347d5bbddc
Albert <kovalevskij.albert@gmail.com>
2015-08-17 14:03:32 +0300

    connected client side

--- app/index.js
+++ app/index.js
@@ -1,10 +1,19 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var status = require('./status.js');
 
+app.use(express.static('public'));
 app.get('/', function(req, res){
   res.send('index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
 io.on('connection', function(socket){
@@ -16,15 +25,6 @@ io.on('connection', function(socket){
       status: data
     });
   });
-
-  socket.on('my other event', function(data) {
-    console.log(data);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
 });
 
 http.listen(3000, function(){
