187b0bca0d33fca59de3e610e24ced79a9f24ce9
Albert <kovalevskij.albert@gmail.com>
2015-08-21 11:03:38 +0300

    refactor status event emitter

--- app/index.js
+++ app/index.js
@@ -2,35 +2,46 @@ var app = require('express')();
 var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var status = require('./status.js');
+var Status = require('./status.js');
 var activate = require('./activate');
 
 app.get('/', function(req, res){
   res.send('index.html');
 });
 var connected = false;
-io.on('connection', function(socket){
+
+var namespace = io.of('/namespace');
+namespace.on('connection', function(socket){
   console.log('a user connected');
+
+  var status = new Status();
 
   if (!connected) {
     connected = true;
-    status.events.emit('subscribe');
+    status.emit('subscribe');
   }
+
+  status.on('data', function(data) {
+    socket.emit('news', {
+      status: data
+    })
+  });
+
+  socket.on('subscribe', function () {
+    if (!connected) {
+      connected = true;
+      status.emit('subscribe');
+    }
+  });
 
   socket.on('activate', function (data) {
     activate.events.emit('activate', data);
   });
 
-  status.events.on('data',function (data){
-    socket.emit('news', {
-//      type:'welcome',
-      status: data
-    });
-  });
-
   socket.on('disconnect', function () {
     if (io.sockets.sockets.length === 0) {
-      status.events.emit('unsubscribe');
+      var status = new Status();
+      status.emit('unsubscribe');
       connected = false;
     }
   });
--- app/status.js
+++ app/status.js
@@ -1,40 +1,45 @@
 var https = require('https');
 var EventEmitter = require('events').EventEmitter;
-
-var events = new EventEmitter();
+var util = require('util');
 
 var statusUrl = 'https://api.sistemium.com/status?json';
 
 var lastData, interval;
 
-function refreshData () {
+var Status = function () {
+  var self = this;
 
-  https.get(statusUrl, function(res) {
-
-    var body = '';
-
-    res.on('data', function (chunk) {
-      body += chunk;
-    });
-
-    res.on('end', function () {
-      var jsonData = JSON.parse(body);
-      //console.log('Got response: ', jsonData);
-      lastData = jsonData;
-      events.emit('data',jsonData);
-    });
-
-  }).on('error', function(e) {
-    console.log('Got error: ', e);
+  this.on('subscribe', function () {
+    interval = setInterval(refreshData, 1000);
+    console.log('subscribe');
   });
-}
-events.on('unsubscribe', function () {
-  console.log('unsubsbcribe');
-  clearInterval(interval);
-});
 
-events.on('subscribe', function () {
-  console.log('subsbcribe');
-  interval = setInterval(refreshData, 1000);
-});
-exports.events = events;
+  this.on('unsubscribe', function () {
+    clearInterval(interval);
+    console.log('unsubscribe');
+  });
+
+  function refreshData () {
+    https.get(statusUrl, function(res) {
+
+      var body = '';
+
+      res.on('data', function (chunk) {
+        body += chunk;
+      });
+
+      res.on('end', function () {
+        var jsonData = JSON.parse(body);
+        //console.log('Got response: ', jsonData);
+        lastData = jsonData;
+        self.emit('data',jsonData);
+      });
+
+    }).on('error', function(e) {
+      console.log('Got error: ', e);
+    });
+  }
+};
+
+util.inherits(Status, EventEmitter);
+module.exports = Status;
