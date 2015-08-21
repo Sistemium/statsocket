53bd60b3274e3f6ea69e78cad0789101db111b59
Albert <kovalevskij.albert@gmail.com>
2015-08-21 15:02:26 +0300

    init check authentication token

--- app/index.js
+++ app/index.js
@@ -12,6 +12,28 @@ app.get('/', function(req, res){
 var statusNS = io.of('/status');
 var status = new Status();
 statusNS.on('connection', function(socket){
+  socket.auth = false;
+  socket.on('authenticate', function(data){
+    //check the auth data sent by the client
+    console.log(data.token);
+
+    //TODO: check token
+    //checkAuthToken(data.token, function(err, success){
+    //  if (!err && success){
+    //    console.log("Authenticated socket ", socket.id);
+    //    socket.auth = true;
+    //  }
+    //});
+  });
+
+  setTimeout(function(){
+    //If the socket didn't authenticate, disconnect it
+    if (!socket.auth) {
+      console.log("Disconnecting socket ", socket.id);
+      socket.disconnect('unauthorized');
+    }
+  }, 1000);
+
   console.log('a user connected');
   status.emit('subscribe');
   status.on('data', function (data) {
